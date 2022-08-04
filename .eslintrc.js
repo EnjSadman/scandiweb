ffe7574e3e4aa84beb740c808f0fd2739a2729f5
Roman Pavlenko <proman9973@gmail.com>
2022-08-04 13:27:18 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  extends: [
+    '@mate-academy/eslint-config-react-typescript',
+    'plugin:react/recommended',
+  ],
+  rules: {
+    // React
+    'react/prop-types': 0,
+    'react/self-closing-comp': 0,
+    'react/display-name': 0,
+
+    // JavaScript
+    semi: 0,
+    'no-proto': 0,
+    'no-unused-vars': 0,
+
+    // TypeScript
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/ban-types': 0,
+    '@typescript-eslint/semi': ['error'],
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'import/no-cycle': 0,
+    'no-console': 0,
+    'no-case-declaration': 0,
+    'prefer-destructing': 0,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+};
